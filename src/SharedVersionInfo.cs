7764e0cd0ad688c193eafb373a53dd657ba701d6
Adrian Lewis <zen_global@hotmail.com>
2019-01-16 22:10:01 +0000

    Built release : 1.0.0-alpha7

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -12,4 +12,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.4+Branch.master.Sha.cc9804e327ca70f9597e6f92a15f37be3388081d")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha.7+Branch.master.Sha.cfe9273a8288e831c45b4cb48e86a1fbff1f3169")]
